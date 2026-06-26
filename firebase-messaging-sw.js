ec88a533acecc72508c91b2e9e23fda2168828c2
courtneyzarzycki-svg <courtney.zarzycki@gmail.com>
2026-06-26 15:04:10 -0700

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,5 @@
 importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js");
-
 firebase.initializeApp({
     apiKey: "AIzaSyA6hEEmGv3X0L5sQbZP2YYUQu-sixDqsPM",
     authDomain: "family-hub-2-4a707.firebaseapp.com",
@@ -10,15 +9,14 @@ firebase.initializeApp({
     messagingSenderId: "533379938918",
     appId: "1:533379938918:web:949fbd193d22ff5b074ba7"
 });
-
 const messaging = firebase.messaging();
-
 messaging.onBackgroundMessage((payload) => {
   self.registration.showNotification(
     payload.notification.title,
     {
       body: payload.notification.body,
-      icon: "/icon.png"
+      icon: "/icon.png",
+      requireInteraction: true
     }
   );
-}); 
+});
